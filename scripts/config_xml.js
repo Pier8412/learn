c36cd6bca98fb004571156f5074c0fb8a2c09994
Pier8412 <106855345+Pier8412@users.noreply.github.com>
2023-02-04 11:40:41 +0800

    Add files via upload

---
+++ scripts/config_xml.js
@@ -0,0 +1,54 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:date="2022-12-01 02:47:09 下午" dc:source="Camtasia,9.0.5,enu" dc:title="化学反应中的有关计算" tscDM:originId="952B5D19-9B51-4445-8DA8-322C87CCB28D" tscDM:project="化学反应中的有关计算">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="456866"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="844" stDim:w="1530"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="en-US">English</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="TableOfContents" xmpDM:frameRate="f1000" xmpDM:trackName="Table of Contents">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:name="引入" xmpDM:startTime="0" tscDM:image="化学反应中的有关计算_Thumbnails.png" tscDM:imageindex="0" tscDM:imageoffset="0" tscDM:imagerect="0, 0, 75, 41"/></rdf:li><rdf:li><rdf:Description xmpDM:name="新课导入" xmpDM:startTime="22433" tscDM:image="化学反应中的有关计算_Thumbnails.png" tscDM:imageindex="1" tscDM:imageoffset="0" tscDM:imagerect="75, 0, 75, 41"/></rdf:li><rdf:li><rdf:Description xmpDM:name="学习目标" xmpDM:startTime="89867" tscDM:image="化学反应中的有关计算_Thumbnails.png" tscDM:imageindex="2" tscDM:imageoffset="0" tscDM:imagerect="150, 0, 75, 41"/></rdf:li><rdf:li><rdf:Description xmpDM:name="化学方程式的含义" xmpDM:startTime="114067" tscDM:image="化学反应中的有关计算_Thumbnails.png" tscDM:imageindex="3" tscDM:imageoffset="0" tscDM:imagerect="225, 0, 75, 41"/></rdf:li><rdf:li><rdf:Description xmpDM:name="计算步骤" xmpDM:startTime="192500" tscDM:image="化学反应中的有关计算_Thumbnails.png" tscDM:imageindex="4" tscDM:imageoffset="0" tscDM:imagerect="300, 0, 75, 41"/></rdf:li><rdf:li><rdf:Description xmpDM:name="拓展应用" xmpDM:startTime="364600" tscDM:image="化学反应中的有关计算_Thumbnails.png" tscDM:imageindex="5" tscDM:imageoffset="0" tscDM:imagerect="375, 0, 75, 41"/></rdf:li><rdf:li><rdf:Description xmpDM:name="课堂小结" xmpDM:startTime="425733" tscDM:image="化学反应中的有关计算_Thumbnails.png" tscDM:imageindex="6" tscDM:imageoffset="0" tscDM:imagerect="450, 0, 75, 41"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Quiz" xmpDM:frameRate="f1000" xmpDM:trackName="Quiz" tscIQ:quizGuid="571CC84D-8742-42D2-B400-3179DDC9317D" tscIQ:authoredEmail="MTc1ODczNTEzM0BxcS5jb20=" tscIQ:requireUserId="0" tscIQ:locale="en-US" tscIQ:reportMethod="APIANDSCORM" tscIQ:allowSkipQuiz="0" tscIQ:clientId="1CDF22D874AC443FBBB68075B4AE31CD" tscIQ:hideReplay="0" tscIQ:quizHash="b2608ff8fab402e1eb89206ae4481142">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:startTime="118800" tscIQ:feedback="1" tscIQ:questionSetName="测验 4"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="MC" tscIQ:id="0"><tscIQ:question>根据水电解的方程式，下列说法正确的是（       ）</tscIQ:question><tscIQ:correctAnswer>4</tscIQ:correctAnswer><tscIQ:answerArray><rdf:Seq><rdf:li><rdf:Description tscIQ:orderId="0"><tscIQ:answer>反应物是水，生成物只有氢气</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="1"><tscIQ:answer>2份水在通电的条件下生成2份氢气和1份氧气</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="2"><tscIQ:answer>每36份质量的水在通电的条件下完全反应生成4份质量的氢气和32份质量的氧气</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="3"><tscIQ:answer>水分子是由两个氢原子和一个氧原子构成</tscIQ:answer></rdf:Description></rdf:li></rdf:Seq></tscIQ:answerArray><tscIQ:feedback><rdf:Bag><rdf:li><rdf:Description tscIQ:reason="correct"><xmpDM:name><rdf:Alt><rdf:li>你真棒！恭喜你答对了！我们继续吧！</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:reason="incorrect"><xmpDM:name><rdf:Alt><rdf:li>很遗憾你答错了！请认真观看吧！</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li></rdf:Bag></tscIQ:feedback></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:startTime="302500" tscIQ:feedback="1" tscIQ:questionSetName="测验 5"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="MC" tscIQ:id="1"><tscIQ:question>根据化学方程式的计算，下列说法正确的是（     ）</tscIQ:question><tscIQ:correctAnswer>8</tscIQ:correctAnswer><tscIQ:answerArray><rdf:Seq><rdf:li><rdf:Description tscIQ:orderId="0"><tscIQ:answer>列比例式的时候可以不带单位</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="1"><tscIQ:answer>设未知量时通常需要带上单位</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="2"><tscIQ:answer>答案可写可不写</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="3"><tscIQ:answer>需要写出正确的化学方程式</tscIQ:answer></rdf:Description></rdf:li></rdf:Seq></tscIQ:answerArray><tscIQ:feedback><rdf:Bag><rdf:li><rdf:Description tscIQ:reason="correct"><xmpDM:name><rdf:Alt><rdf:li>你真棒！我们继续前进！</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:reason="incorrect" tscIQ:jumpToTime="192500"><xmpDM:name><rdf:Alt><rdf:li>很遗憾你答错了！再温习一遍吧！</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li></rdf:Bag></tscIQ:feedback></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li><rdf:li><rdf:Description xmpDM:startTime="315333" tscIQ:feedback="1" tscIQ:questionSetName="测验 6"><tscIQ:questions><rdf:Seq><rdf:li><rdf:Description tscIQ:type="MC" tscIQ:id="2"><tscIQ:question>小优同学解题步骤有几处错误？</tscIQ:question><tscIQ:correctAnswer>4</tscIQ:correctAnswer><tscIQ:answerArray><rdf:Seq><rdf:li><rdf:Description tscIQ:orderId="0"><tscIQ:answer>5</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="1"><tscIQ:answer>6</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="2"><tscIQ:answer>7</tscIQ:answer></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:orderId="3"><tscIQ:answer>8</tscIQ:answer></rdf:Description></rdf:li></rdf:Seq></tscIQ:answerArray><tscIQ:feedback><rdf:Bag><rdf:li><rdf:Description tscIQ:reason="correct"><xmpDM:name><rdf:Alt><rdf:li>恭喜你答对了！你真是火眼金睛啊！</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li><rdf:li><rdf:Description tscIQ:reason="incorrect"><xmpDM:name><rdf:Alt><rdf:li>还需要继续努力！加油！</rdf:li></rdf:Alt></xmpDM:name></rdf:Description></rdf:li></rdf:Bag></tscIQ:feedback></rdf:Description></rdf:li></rdf:Seq></tscIQ:questions></rdf:Description></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                     <tscIQ:QuizParams><rdf:Bag><rdf:li xmpDM:name="txtPrev" xmpDM:value="上一个"/><rdf:li xmpDM:name="txtNext" xmpDM:value="下一个"/><rdf:li xmpDM:name="txtAnswerQuestion" xmpDM:value="检测"/><rdf:li xmpDM:name="txtSubmit" xmpDM:value="提交"/><rdf:li xmpDM:name="txtReview" xmpDM:value="回放前面内容"/><rdf:li xmpDM:name="txtReviewAnswer" xmpDM:value="查看答案"/><rdf:li xmpDM:name="txtContinue" xmpDM:value="继续观看"/></rdf:Bag></tscIQ:QuizParams></rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="true"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="true"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="true"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="en-US"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="化学反应中的有关计算.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="化学反应中的有关计算.zip"/><rdf:li xmpDM:name="2" xmpDM:value="化学反应中的有关计算_Thumbnails.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
